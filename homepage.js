ecdd821bdae3d74cb531663f3ce2bac167cf09e4
siddharthshirode1-blip <siddharthshirode1@gmail.com>
2026-03-25 16:02:58 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
     $("#card1").mouseenter(function(){
         $("#cardimg").css({
-            "background-image": "url('/Images/Morse_code.webp')"
+            "background-image": "url('Images/Morse_code.webp')"
         })
         $("#cardinfo").html("<strong>Morse_Code:</strong><br><p>Learn or Convert into the language of dot and dash")
     });
 
     $("#card2").mouseenter(function(){
         $("#cardimg").css({
-            "background-image": "url('/Images/Binary.png')"
+            "background-image": "url('Images/Binary.png')"
         })
         $("#cardinfo").html("<h1>Locked</h1><br><strong>Binary:</strong><br><p>Learn or Convert into 1 & 0's")
     })
 
     $("#card3").mouseenter(function(){
         $("#cardimg").css({
-            "background-image": "url('/Images/Touch_typing.webp')"
+            "background-image": "url('Images/Touch_typing.webp')"
         })
         $("#cardinfo").html("<h1>Locked</h1><br><strong>TouchTying:</strong><br><p>Learn and Type as a professional")
     })
